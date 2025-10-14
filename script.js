6455e8439758b472f4428829f4e2b9df4c078c02
tania.barillaro <nanacampus2025@gmail.com>
2025-10-14 14:50:54 +0200

    pseudo code movements player

---
+++ script.js
@@ -0,0 +1,9 @@
+// déplacement du joueur avec les touches fléchées
+
+// définir les mouvements possibles (switch case)
+    // aller à la cellule au-dessus
+    // aller à la cellule en dessous
+    // aller à la cellule à gauche
+    // aller à la cellule à droite
+
+// déplacer le joueur si la cellule cible est valide
